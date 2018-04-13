f91af0d59cedda206b5ea147e4657047a135f165
EpicEmaraty <35786778+EpicEmaraty@users.noreply.github.com>
2018-04-12 19:43:02 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <vector>
+#include <cstdlib>
+#include <ctime>
 
 using namespace std;
 
@@ -7,63 +9,85 @@ const int MAX_PERIODS = 10;
 const int MAX_TEAMS = 4;
 const int MIN_PERIODS = 1;
 const int MIN_TEAMS = 1;
+void printScoreboard( vector < vector < int > >);
+int randomBetween (int,int); 
 
 int main()
 {
-  int periods;
-  int teams;
-  int playerCount = 1;
+        srand((int) time(0));
+        int periods, row;
+        int teams, col;
+        int P_NUMBER = 1;
+        vector < vector <int> > score_Board;
 
-  vector<vector<int> > board;
+        cout<<"How many competitors? ";
+        cin>>teams;
+        cout<<"How many scoring periods? ";
+        cin>>periods;
 
-  cout<<"How many competitors? ";
-  cin>>teams;
-  cout<<"How many scoring periods? ";
-  cin>>periods;
 
-  if( teams < MIN_TEAMS || periods < MIN_PERIODS || 
-      teams > MAX_TEAMS || periods > MAX_PERIODS )
-  {
-    cout<<"Must have between "<<MIN_TEAMS<<" and "<<MAX_TEAMS<<" competitors.\n";
-    cout<<"and between "<<MIN_PERIODS<<" and "<<MAX_PERIODS<<" periods.\n";
-    return 0;
-  }
-  else
-  {
-    board.resize(teams);
 
-    for(int r=0; r<board.size(); r++)
-    {
-      board[r].resize(periods);
-    }
-  }
 
-  //make scoreboard and fill it with zeros
-  
-  for(int r=0; r<board.size(); r++)
-  {
-    for(int c=0; c<board[r].size(); c++)
-    {
-      board[r][c] = 0;
-    }
-  }
 
-   //once created, display the scoreboard
-  
-  cout<<"SCOREBOARD\n";
+        if( teams < MIN_TEAMS || periods < MIN_PERIODS || 
+                        teams > MAX_TEAMS || periods > MAX_PERIODS )
+        {
+                cout<<"Must have between "<<MIN_TEAMS<<" and "<<MAX_TEAMS<<" competitors.\n";
+                cout<<"and between "<<MIN_PERIODS<<" and "<<MAX_PERIODS<<" periods.\n";
+                return 0;
+        }
+        else
+        {
+                score_Board.resize(teams);
+                for( int row = 0; row < score_Board.size(); row ++)
+                {
+                        score_Board[row].resize(periods);
+                }
 
-  for(int r=0; r<board.size(); r++)
-  { 
-    cout<<"Player "<< playerCount <<": ";
-    for(int c=0; c<board[r].size(); c++)
-    {
-      cout<< board[r][c] <<"|";
-    }
-    cout<<endl;
-    playerCount++;
-  }
-   
-  
-  
-  return 0;
+                printScoreboard(score_Board);
+
+                for(int row = 0; row < score_Board.size(); row ++)
+                {
+                        for(int col = 0; col < score_Board[row].size(); col ++)
+                        {
+                                score_Board [row][col] = randomBetween(5,5);
+                        }
+                }
+
+                printScoreboard(score_Board);
+
+                //make scoreboard and fill it with zeros
+
+                //once created, display the scoreboard
+        }
+        return 0;
 }
+
+void printScoreboard( vector < vector <int> > grid)
+{   
+        cout<< "SCOREBOARD\n";
+
+        for(int row = 0; row < grid.size(); row++)
+        {
+                cout<< "Player " <<row + 1<< ": ";
+                for(int col = 0; col < grid[row].size(); col ++)
+                {
+                        cout<< grid[row][col];
+                        cout<< "|";
+                }
+                cout<<endl;
+        }
+}
+int randomBetween(int first, int second)
+{
+        if( first > second)
+        {
+                return second + rand()%(first-second + 1);
+        }
+        else 
+        {
+                return first + rand()%(second-first + 1);
+        }
+}
+
+
